95df297fd292a260a564f43a1628d5ae0ca5f232
Jonathan Castro <jacr96@gmail.com>
2020-03-03 23:11:49 -0600

    Updated view

---
+++ resources/lang/en/commons.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Commons Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the common library to build
+    | the common words used along the whole application.
+    |
+    */
+    
+    'appName' => 'Portal',
+    'title' => 'Registro de Incidencias',
+    'email' => 'Email',
+    'password' => 'Password',
+    'rememberme' => 'Remember me',
+    'login' => 'Login',
+    'forgotPass' => 'Forgot Your Password?',
+    'logout' => 'Logout',
+    'register' => 'Register',
+    'home' => 'Home',
+    'siteMap' => 'Site Map',
+];
