9e87a8ed8f23aa1d5c32c9a8cacf6162e4e9c5a0
Alvaro <asalazarr430@ulacit.ed.cr>
2020-03-04 18:32:45 -0600

    Changes on register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -68,7 +68,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Province') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('commons.province') }}</label>
 
                             <div class="col-md-6">
                                 <input id="Province" type="text" class="form-control @error('Province') is-invalid @enderror" name="Province" value="{{ old('Province') }}" required autocomplete="Province" autofocus>
