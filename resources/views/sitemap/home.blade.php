7167025911c90a2995abd7257730aaaa65809d5f
Alvaro <asalazarr430@ulacit.ed.cr>
2020-04-14 19:57:59 -0600

    home.blade.php new changes in method to show sitemap in /home

--- resources/views/sitemap/home.blade.php
+++ resources/views/sitemap/home.blade.php
@@ -1,9 +1,9 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @foreach ($home as $content)
+    @foreach ($home as $homeIndex)
         <url>
-            <loc>http://localhost/home/{{ $content->slug }}</loc>
-            <lastmod>{{ $content->created_at->tz('UTC')->toAtomString() }}</lastmod>
+            <loc>http://localhost/home/{{ $homeIndex->slug }}</loc>
+            <lastmod>{{ $homeIndex->created_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.9</priority>
         </url>
